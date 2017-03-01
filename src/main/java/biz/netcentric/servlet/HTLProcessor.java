45f1d2aed167d9c8163a3be53f899f5b11de7199
Jhoan Munoz <jmunoz@pros.com>
2017-03-01 12:07:30 -0500

    Slightly-003: Run JS code from HTML file - Add Rhino as dependency in order to use it to execute JS code in the servlet - Make changes to find the script tag in the HTML file and execute its code

--- src/main/java/biz/netcentric/servlet/HTLProcessor.java
+++ src/main/java/biz/netcentric/servlet/HTLProcessor.java
@@ -13,14 +13,24 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ImporterTopLevel;
+import org.mozilla.javascript.RhinoException;
+import org.mozilla.javascript.ScriptableObject;
 
 public class HTLProcessor extends HttpServlet
 {
+    private static final String CHARSET_NAME = "UTF-8";
     private static final String DEFAULT_PATH = "/";
     private static final String INDEX_PATH = "/index.html";
+    private static final String JS_ATTR_VAL = "server/javascript";
+    private static final String JS_SOURCE_NAME = "<code>";
+    private static final String REQUEST_OBJ_KEY = "request";
+    private static final String TYPE_ATTR_KEY = "type";
 
     private ServletConfig servletConfig;
 
+    @Override
     public void init(ServletConfig config)
     throws ServletException
     {
@@ -28,7 +38,7 @@ public class HTLProcessor extends HttpServlet
         servletConfig = config;
     }
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
     {
         StringBuilder responseBuilder = new StringBuilder();
@@ -39,8 +49,16 @@ public class HTLProcessor extends HttpServlet
         {
             ServletContext servletContext = servletConfig.getServletContext();
             File htmlFile = new File(servletContext.getResource(filePath).toURI());
-            Document htmlDoc = Jsoup.parse(htmlFile, "UTF-8");
+            Document htmlDoc = Jsoup.parse(htmlFile, CHARSET_NAME);
 
+            String jsCode = htmlDoc.getElementsByAttributeValue(TYPE_ATTR_KEY, JS_ATTR_VAL).html();
+            Context context = Context.enter();
+            ScriptableObject scope = new ImporterTopLevel(context);
+            scope.put(REQUEST_OBJ_KEY, scope, request);
+            context.evaluateString(scope, jsCode, JS_SOURCE_NAME, 1, null);
+
+            // TODO create a helper methods to find the special HTML tags and process them using the
+            //      JS objects
             responseBuilder.append("<pre>" + htmlDoc.html() + "</pre>");
         }
         catch (FileNotFoundException | NullPointerException exception)
@@ -55,9 +73,17 @@ public class HTLProcessor extends HttpServlet
             responseBuilder.append("The provided URL is not correctly formed");
             use.printStackTrace();
         }
+        catch (RhinoException rhe)
+        {
+            responseBuilder.delete(0, responseBuilder.length());
+            responseBuilder.append("There's an error in the javascript code in the requested page.");
+            responseBuilder.append(rhe.getMessage());
+            rhe.printStackTrace();
+        }
         finally
         {
             printResponse(response, responseBuilder.toString());
+            Context.exit();
         }
     }
 
