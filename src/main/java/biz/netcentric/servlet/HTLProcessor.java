31fe9cc910e35b5fe7b98088b668fc8d32ed6831
Jhoan Munoz <jmunoz@pros.com>
2017-03-01 23:57:17 -0500

    Slightly-008: Add documentation - Add Javadoc for the servlet class - Add Maven-Javadoc plugin

--- src/main/java/biz/netcentric/servlet/HTLProcessor.java
+++ src/main/java/biz/netcentric/servlet/HTLProcessor.java
@@ -24,24 +24,80 @@ import org.mozilla.javascript.NativeJavaObject;
 import org.mozilla.javascript.RhinoException;
 import org.mozilla.javascript.ScriptableObject;
 
+/**
+ * Servlet that receives requests for HTML documents. These documents are expected to be templates
+ * that may contain three types of expressions:
+ * <ul>
+ *     <li><b>data-if expressions:</b> Elements that are displayed based on a boolean value. These
+ *     kind of elements are evaluated by the method
+ *     {@link #evaluateIfExpressions(Document, Context, ScriptableObject)}.</li>
+ *     <li><b>data-for-x expressions:</b> Elements that iterate over the specified list and render
+ *     as many elements as the list has. These kind of elements are evaluated by the method
+ *     {@link #evaluateForExpressions(Document, Context, ScriptableObject)}</li>
+ *     <li><b>$-expressions:</b></li> Elements contained inside the ${ } characters which content is
+ *     evaluated by the javascript engine. These kind of elements are evaluated by the method
+ *     {@link #evaluateExpressions(Document, Context, ScriptableObject)}
+ * </ul>
+ *
+ * The servlet is also capable of evaluate any Javascript code embedded inside a {@code <script>}
+ * tag with the {@code type} attribute set to {@code "server/javascript"}.
+ */
 public class HTLProcessor extends HttpServlet
 {
+    /** Charset used to load the HTML document. */
     private static final String CHARSET_NAME = "UTF-8";
-    private static final String DEFAULT_PATH = "/";
-    private static final String DATA_FOR_ATTR_PREFIX = "data-for";
-    private static final int DATA_FOR_X_IDX = 2;
-    private static final String DATA_IF_ATTR_NAME = "data-if";
-    private static final String EXPR_PREFIX = "${";
-    private static final String EXPR_SUFFIX = "}";
-    private static final String INDEX_PATH = "/index.html";
-    private static final String JS_ATTR_VAL = "server/javascript";
-    private static final String JS_SOURCE_NAME = "<code>";
-    private static final String REQUEST_OBJ_KEY = "request";
-    private static final String RESPONSE_CONTENT_TYPE = "text/html;charset=UTF-8";
-    private static final String TYPE_ATTR_KEY = "type";
 
+    /** Default path received by the servlet when no file is specified. */
+    private static final String DEFAULT_PATH = "/";
+
+    /** Prefix of the attribute used for data-for-x expressions. */
+    private static final String DATA_FOR_ATTR_PREFIX = "data-for";
+
+    /** Index of the "x" variable in the data-for-x expressions. */
+    private static final int DATA_FOR_X_IDX = 2;
+
+    /** Name of the attribute used for data-if expressions. */
+    private static final String DATA_IF_ATTR_NAME = "data-if";
+
+    /** Prefix used for $-expressions. */
+    private static final String EXPR_PREFIX = "${";
+
+    /** Suffix used for $-expressions. */
+    private static final String EXPR_SUFFIX = "}";
+
+    /** Path for the index.html file. */
+    private static final String INDEX_PATH = "/index.html";
+
+    /**
+     * Name of the attribute expected to be used in the {@code <script>} tag containing the
+     * Javascript code to be executed by the servlet
+     */
+    private static final String JS_ATTR_NAME = "type";
+
+    /**
+     * Value for the attribute expected to be used in the {@code <script>} tag containing the
+     * Javascript code to be executed by the servlet.
+     */
+    private static final String JS_ATTR_VAL = "server/javascript";
+
+    /** Reference name used to execute Javascript code in the engine. */
+    private static final String JS_SOURCE_NAME = "<code>";
+
+    /** Key used to store the request object in the Javascript execution scope. */
+    private static final String REQUEST_OBJ_KEY = "request";
+
+    /** Content type used for the Servlet response. */
+    private static final String RESPONSE_CONTENT_TYPE = "text/html;charset=UTF-8";
+
+    /** {@link javax.servlet.ServletConfig} instance used to get the servlet context. */
     private ServletConfig servletConfig;
 
+    /**
+     * Stores the given configuration object in the class variable so it can be used later.
+     *
+     * @param config Servlet configuration object
+     * @throws ServletException If there's any problem while initiating the servlet.
+     */
     @Override
     public void init(ServletConfig config)
     throws ServletException
@@ -50,6 +106,27 @@ public class HTLProcessor extends HttpServlet
         servletConfig = config;
     }
 
+    /**
+     * Processes the given request by performing the following steps:
+     * <ol>
+     *     <li>Load the requested HTML document based on the request path</li>
+     *     <li>Evaluate Javascript code in the specified HTML document. The servlet will only
+     *     evaluate code inside a {@code <script>} tag with the {@code type} attribute set to
+     *     {@code "server/javascript"}.</li>
+     *     <li>Evaluate data-if expressions</li>
+     *     <li>Evaluate data-for-x expressions</li>
+     *     <li>Evaluate $-expressions</li>
+     *     <li>Print out the response</li>
+     * </ol>
+     *
+     * If there's any problem during the execution of the steps above, the exception will be caught
+     * and an error message will be returned as response.
+     *
+     * @param request Object containing the request information
+     * @param response Object containing the response information
+     * @throws ServletException If there's any exception not treated during the request processing.
+     * @throws IOException If there's any problem trying to load the specified HTML document.
+     */
     void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
     {
@@ -63,7 +140,8 @@ public class HTLProcessor extends HttpServlet
             File htmlFile = new File(servletContext.getResource(filePath).toURI());
             Document htmlDoc = Jsoup.parse(htmlFile, CHARSET_NAME);
 
-            String jsCode = htmlDoc.getElementsByAttributeValue(TYPE_ATTR_KEY, JS_ATTR_VAL).html();
+            // Evaluate Javascript code
+            String jsCode = htmlDoc.getElementsByAttributeValue(JS_ATTR_NAME, JS_ATTR_VAL).html();
             Context context = Context.enter();
             ScriptableObject scope = new ImporterTopLevel(context);
             scope.put(REQUEST_OBJ_KEY, scope, request);
@@ -99,6 +177,16 @@ public class HTLProcessor extends HttpServlet
         }
     }
 
+    /**
+     * Evaluates the data-if expressions. It looks for all the expressions of this kind in the HTML
+     * document and evaluates its content using the Javascript engine. If the evaluation result is
+     * true, the element is preserved; otherwise, it's removed. In any case, the data-if attribute
+     * is removed from the element.
+     *
+     * @param htmlDoc HTML document requested by the user
+     * @param context Javascript evaluation context
+     * @param scope Javascript evaluation scope
+     */
     void evaluateIfExpressions(Document htmlDoc, Context context, ScriptableObject scope)
     {
         Elements dataIfElements = htmlDoc.getElementsByAttributeStarting(DATA_IF_ATTR_NAME);
@@ -116,6 +204,22 @@ public class HTLProcessor extends HttpServlet
         });
     }
 
+    /**
+     * Evaluates the data-for-x expressions by following these steps:
+     * <ol>
+     *     <li>Look for all the elements containing attributes that start with the
+     *     {@link #DATA_FOR_ATTR_PREFIX} prefix</li>
+     *     <li>Identify the "x" value </li>
+     *     <li>Evaluate the Javascript expression corresponding to the elements to display</li>
+     *     <li>Generate as many elements as the list has and replace the "x" value with the actual
+     *     values from the list</li>
+     *     <li>Remove the data-for-x attribute so clean HTML is returned</li>
+     * </ol>
+     *
+     * @param htmlDoc HTML document requested by the user
+     * @param context Javascript evaluation context
+     * @param scope Javascript evaluation scope
+     */
     void evaluateForExpressions(Document htmlDoc, Context context, ScriptableObject scope)
     {
         Elements dataIfElements = htmlDoc.getElementsByAttributeStarting(DATA_FOR_ATTR_PREFIX);
@@ -140,6 +244,18 @@ public class HTLProcessor extends HttpServlet
         });
     }
 
+    /**
+     * Evaluates the $-expressions. To evaluate this expressions, the HTML code is analyzed to find
+     * the occurrences of elements with the {@code ${x}} pattern. Each ocurrence is evaluated using
+     * the Javascript engine and its value is placed in the document.
+     *
+     * @param htmlDoc HTML document requested by the user
+     * @param context Javascript evaluation context
+     * @param scope Javascript evaluation scope
+     *
+     * @return A string representation of the HTML document with the $-expressions replaced by their
+     *         actual values.
+     */
     String evaluateExpressions(Document htmlDoc, Context context, ScriptableObject scope)
     {
         String html = htmlDoc.html();
@@ -157,6 +273,15 @@ public class HTLProcessor extends HttpServlet
         return StringUtils.join(expressions);
     }
 
+    /**
+     * Method executed when the servlet receives a POST request. Right now it'll always execute the
+     * {@link #processRequest(HttpServletRequest, HttpServletResponse)} method.
+     *
+     * @param request Object containing the request information
+     * @param response Object containing the response information
+     * @throws ServletException If there's any exception not treated during the request processing.
+     * @throws IOException If there's any problem trying to load the specified HTML document.
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
@@ -164,6 +289,15 @@ public class HTLProcessor extends HttpServlet
         processRequest(request, response);
     }
 
+    /**
+     * Method executed when the servlet receives a GET request. Right now it'll always execute the
+     * {@link #processRequest(HttpServletRequest, HttpServletResponse)} method.
+     *
+     * @param request Object containing the request information
+     * @param response Object containing the response information
+     * @throws ServletException If there's any exception not treated during the request processing.
+     * @throws IOException If there's any problem trying to load the specified HTML document.
+     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
@@ -171,6 +305,14 @@ public class HTLProcessor extends HttpServlet
         processRequest(request, response);
     }
 
+    /**
+     * Prints the servlet response.
+     *
+     * @param response Object containing the response information
+     * @param text Text to be printed as response.
+     *
+     * @throws IOException If there's any problem trying to print the response.
+     */
     private void printResponse(HttpServletResponse response, String text)
     throws IOException
     {
